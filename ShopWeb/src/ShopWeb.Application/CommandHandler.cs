9e0781af4a6146d9c2250e666f24c760a3f38b92
Matheus Reis <reis.mmatheus@gmail.com>
2022-09-20 23:42:52 -0300

    Adding CQRS Structure

---
+++ ShopWeb/src/ShopWeb.Application/CommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using ShopWeb.Infra.Data.UnitOfWork;
+
+namespace ShopWeb.Application
+{
+    public class CommandHandler
+    {
+        private readonly IMediator _mediator;
+        private readonly IUnitOfWork _unitOfWork;
+        public CommandHandler(IMediator mediator, IUnitOfWork unitOfWork)
+        {
+            _mediator = mediator;
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool Commit()
+        {
+            return _unitOfWork.Commit();
+        }
+
+        public void PublishEvents(IList<object> events)
+        {
+            events.ToList().ForEach(e => _mediator.Publish(e));
+        }
+    }
+}
---
+++ ShopWeb/src/ShopWeb.Infra.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.Infra.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        bool Commit();
+    }
+}
