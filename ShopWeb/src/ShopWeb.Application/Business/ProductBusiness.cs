4bae222fa9b573c4fd552d0bd504ebf4de6d78ac
Matheus Reis <reis.mmatheus@gmail.com>
2022-10-09 22:10:14 -0300

    Add Product Business and Commands

---
+++ ShopWeb/src/ShopWeb.Application/Business/ProductBusiness.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using ShopWeb.Application.Interface;
+
+namespace ShopWeb.Application.Business
+{
+    public class ProductBusiness : IProductBusiness
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ProductBusiness(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> CreateAsync()
+        {
+            //var command = _mapper.Map<CreateProductCommand>(product);
+            //var result = await _mediator.Send(command);
+            return true;
+        }
+    }
+}
---
+++ ShopWeb/src/ShopWeb.Domain/Commands/Products/Add/AddProductCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.Domain.Commands.Products.Add
+{
+    public class AddProductCommandHandler : IRequestHandler<AddProductCommand, int>
+    {
+        public Task<int> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
