b503cece033ff41551c1d516c5478dd8425eaf20
Matheus Reis <reis.mmatheus@gmail.com>
2022-10-13 21:28:18 -0300

    Adding Purchase

---
+++ ShopWeb/src/ShopWeb.Domain/Entity/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWeb.Domain.Entity
+{
+    public class Purchase : Entity
+    {
+        public Guid CustomerId { get; set; }
+        public ICollection<PurchaseProduct> PurchaseProducts { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
